a7d91d40543708ca9d7aea2129fbda90473ca232
Eduastras <eduastras@outlook.com>
2026-01-26 03:18:46 -0300

    identificando o método HTTP

--- API/api01/src/server.js
+++ API/api01/src/server.js
@@ -1,8 +1,10 @@
 import http from "http"
 
-const server = http.createServer((req, res) => {
-    
-    return res.end("Minha primeira API!")
+const server = http.createServer((request, response) => {
+    // request é um objeto (response tambem) com diversas propriedades, e uma delas é o method, do qual podemos ver qual é o método HTTP que foi requisitado.
+    // Aqui estamos desestruturando o request, que é um objeto. (pegando a propriedade method do request e passando pra variavel metodo.)
+    const { method: metodo } = request
+    return response.end(`Metodo: ${metodo}`)
 })
 
 server.listen(3333)
