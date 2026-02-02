c127c8e63bb380f447d9d9c7c86054409c3385c2
Eduastras <eduastras@outlook.com>
2026-02-02 03:38:25 -0300

    criando o arquivo para guardar dados

---
+++ API/database/api011/src/database.js
@@ -0,0 +1,35 @@
+// pacote do próprio node para lidar com arquivos (files).
+import fs from "node:fs/promises"
+
+// Estamos criando uma nova URL, o primeiro parâmetro é nome do arquivo (caminho relativo), e o segundo o endereço da URL.
+const DATABASE_PATH = new URL("db.json", import.meta.url)
+
+// Class para Banco de Dados.
+export class Database {
+    // Criamos uma propriedade database que recebe um objeto vazio.
+    database = {}
+
+    constructor() {
+        this.persist()
+    }
+
+    persist() {
+        // Usando o pacote fs, para criar um arquivo baseado na URL que criamos, e no arquivo mandamos o database convertido em objeto JSON.
+        fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+    }
+
+    // Método que recebe 2 propriedades.
+    insert(table, data) {
+        if(Array.isArray(this.database[table])) {
+            this.database[table].push(data)
+        } else {
+            this.database[table] = [data]
+        }
+    }
+
+    // aqui é um método que recebe um parâmetro.
+    select(table) {
+
+        return this.database[table]
+    }
+}
